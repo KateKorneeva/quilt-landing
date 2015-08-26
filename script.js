0b7c301086150358f0d82a68c3f7189bf85c9132
korneed <korneevaed@gmail.com>
2015-08-26 22:37:02 +0200

    So creationg of layout began. So far it works

---
+++ script.js
@@ -0,0 +1,20 @@
+// Sticky Header
+// $(window).scroll(function() {
+
+//     if ($(window).scrollTop() > 100) {
+//         $('.head').addClass('sticky');
+//     } else {
+//         $('.head').removeClass('sticky');
+//     }
+// });
+
+// Navigation Scroll
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
