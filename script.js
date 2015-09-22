718df1c3fb48e11c2323e0bf917f653d2abcbbea
Ekaterina Korneeva <ekaterina.korneeva@home24.de>
2015-09-22 18:40:35 +0200

    added buttons, started auto-filling for order

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-// Sticky Header
-// $(window).scroll(function() {
+$(document).ready(function() {
 
-//     if ($(window).scrollTop() > 100) {
-//         $('.head').addClass('sticky');
-//     } else {
-//         $('.head').removeClass('sticky');
-//     }
-// });
-
-// Navigation Scroll
-$('nav a').click(function(event) {
-    var id = $(this).attr("href");
-    var offset = 70;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({
-        scrollTop: target
-    }, 500);
-    event.preventDefault();
-    console.log("dsfsdfsfsf");
-});
+  $(".js-add-color").click(function() {
+  	$(".js-color").val($(this).siblings().text());
+  });
+})
