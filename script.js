d352cc3d3bacdc1c876da8f3619e3f473d8db0de
Ekaterina Korneeva <ekaterina.korneeva@home24.de>
2015-10-08 16:56:02 +0200

    added events to make sections visible

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
 
-$(".js-color").val("");
+	// Add to order
 
-    $(".js-add-color").click(function() {
+	$(".js-color").val("");
 
-      if ( $(".js-color").val().length == 0 ) {
-          $(".js-color").val($('.js-color').val() + $(this).siblings().text() );
-      }
-      else {
-        $(".js-color").val( $('.js-color').val() + ", " + $(this).siblings().text() );
-      }
+	$(".js-add-color").click(function () {
 
-    });
+		if ( $(".js-color").val().length == 0 ) {
+				$(".js-color").val($('.js-color').val() + $(this).siblings().text() );
+		}
+		else {
+			$(".js-color").val( $('.js-color').val() + ", " + $(this).siblings().text() );
+		}
+
+	});
+
+	// end of Add to order
+
+	// Make sections visible
+
+	$(".js-call-calc").click(function () {
+		$(".js-calculation").removeClass("hidden"); // make calculation visible
+	});
+
+	$(".js-call-advan").click(function () {
+		$(".js-advantages").removeClass("hidden"); // make advantages visible
+	});
 });
