dbb236442a3db6a8fe8a8fda593be47defa64b8f
Ekaterina Korneeva <ekaterina.korneeva@home24.de>
2015-10-08 18:19:59 +0200

    gallery works now somwhow!

--- script.js
+++ script.js
@@ -15,15 +15,32 @@ $(document).ready(function() {
 
 	});
 
-	// end of Add to order
+	// end of "Add to order"
+
+
 
 	// Make sections visible
 
+	// make calculation visible
 	$(".js-call-calc").click(function () {
-		$(".js-calculation").removeClass("hidden"); // make calculation visible
+		$(".js-calculation").removeClass("hidden");
 	});
 
+	// make advantages visible
 	$(".js-call-advan").click(function () {
-		$(".js-advantages").removeClass("hidden"); // make advantages visible
+		$(".js-advantages").removeClass("hidden");
 	});
+
+	// show certificate
+	$(".js-show-cert").click(function () {
+		// incert gallery image here with the certificate
+	});
+
+	// end of "Make sections visible"
+
+
+
+	// Gallery
+
+	
 });
